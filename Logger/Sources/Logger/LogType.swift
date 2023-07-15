5647d65f954c17c97feccc9a63187d27eb7e5db6
alper12 <alper@getlipa.com>
2023-07-15 14:37:15 +0300

    Project setup

---
+++ Logger/Sources/Logger/LogType.swift
@@ -0,0 +1,12 @@
+//
+//  LogType.swift
+//  
+//
+//  Created by Alper Ozturk on 15.7.23..
+//
+
+import Foundation
+
+public enum LogType {
+    case error, info, warning, action, success, withoutIcon
+}
---
+++ Logger/Sources/Logger/Logger.swift
@@ -0,0 +1,44 @@
+
+import Foundation
+import os
+
+public struct Logger {
+    private let onlyLogInDebugMode: Bool
+    
+    public init(onlyLogInDebugMode: Bool) {
+        self.onlyLogInDebugMode = onlyLogInDebugMode
+    }
+    
+    private let osLogger: os.Logger = .init(subsystem: Bundle.main.bundleIdentifier!, category: "lipa")
+    
+    private var shouldLog: Bool {
+        #if DEBUG
+            return true
+        #else
+            return onlyLogInDebugMode
+        #endif
+    }
+}
+
+extension Logger {
+    public func log(type: LogType, message: String) {
+        guard shouldLog else {
+            return
+        }
+        
+        switch type {
+        case .error:
+            osLogger.critical("\("🆘 " + message)")
+        case .info:
+            osLogger.info("\("ℹ️ " + message)")
+        case .warning:
+            osLogger.warning("\("⚠️ " + message)")
+        case .action:
+            osLogger.notice("\("🔵 " + message)")
+        case .success:
+            osLogger.notice("\("✅ " + message)")
+        case .withoutIcon:
+            print(message)
+        }
+    }
+}
